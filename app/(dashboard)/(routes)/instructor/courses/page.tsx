292465fa848fa65eadc32e95972f6c2a60d3e65f
Michael Owuor <michael.owuor@gmail.com>
2024-03-24 23:32:40 +0300

    feat: Add instructor courses table columns and data table component
    - Added new file `columns.tsx` with column definitions for the instructor courses table
    - Added new file `data-table.tsx` with a data table component to display the courses
    - Updated `page.tsx` to import and use the data table component with the provided columns and course data

--- app/(dashboard)/(routes)/instructor/courses/page.tsx
+++ app/(dashboard)/(routes)/instructor/courses/page.tsx
@@ -1,12 +1,30 @@
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
-const CoursesPage = () => {
+import { db } from "@/lib/data/db";
+
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+
+const CoursesPage = async () => {
+	const { userId } = auth();
+
+	if (!userId) {
+		return redirect("/");
+	}
+
+	const courses = await db.course.findMany({
+		where: {
+			userId,
+		},
+		orderBy: {
+			createdAt: "desc",
+		},
+	});
+
 	return (
 		<div className="p-6">
-			<Button asChild>
-				<Link href="/instructor/create">New Course</Link>
-			</Button>
+			<DataTable columns={columns} data={courses} />
 		</div>
 	);
 };
