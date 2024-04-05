eeeb0046b85060ec3175c1963afdd2b7c67a7f49
Michael Caesar Owuor <michael.owuor@gmail.com>
2024-04-06 00:23:13 +0300

    feat: Add instructor authentication to instructor layout (#17)
    This commit adds instructor authentication to the instructor layout in the dashboard. Now, only users with an instructor role will be able to access this layout. If a user without the instructor role tries to access it, they will be redirected to the homepage.

--- app/(dashboard)/(routes)/instructor/layout.tsx
+++ app/(dashboard)/(routes)/instructor/layout.tsx
@@ -1,4 +1,15 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
+import { isInstructor } from "@/lib/instructor";
+
 const InstructorLayout = ({ children }: { children: React.ReactNode }) => {
+	const { userId } = auth();
+
+	if (!isInstructor(userId)) {
+		return redirect("/");
+	}
+
 	return <main>{children}</main>;
 };
 
