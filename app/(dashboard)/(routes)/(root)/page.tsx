a327aaaedab53f6f6e1f4a3c9e96a994c770235e
Michael Owuor <michael.owuor@gmail.com>
2024-04-05 23:16:28 +0300

    feat: Add InformationCard component
    - Added a new file `information-card.tsx` in the `_components` folder.
    - The `InformationCard` component displays information with an icon, label, and number of items.
    - It accepts props such as `variant`, `icon`, `numberOfItems`, and `label`.
    - The component is styled using Tailwind CSS classes.
    
    fix: Update HomePage component
    
    - Imported necessary dependencies and components.
    - Updated the function signature to be asynchronous.
    - Added logic to redirect to the homepage if there is no user ID.
    - Retrieved dashboard courses using the `getDashboardCourses` function.
    - Rendered two instances of the `InformationCard` component with different icons, labels, and numbers of items.
    - Passed data from the API response to the `CoursesList` component.
    
    refactor: Rename types in get-dashboard-courses.ts
    
    - Renamed types for better clarity and consistency:
      - Changed `CourseWithProgressWithCategory` to `CourseWithProgressAndCategory`.
      - Changed variable names accordingly throughout the file.
    
    docs: Update README.md
    
    - Updated documentation for better readability and accuracy.

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,4 +1,41 @@
-const HomePage = () => {
-	return <div>Home Page</div>;
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+import { CheckCircle, Clock } from "lucide-react";
+
+import { getDashboardCourses } from "@/lib/actions/get-dashboard-courses";
+
+import { CoursesList } from "@/components/courses-list";
+import { InformationCard } from "./_components/information-card";
+
+const HomePage = async () => {
+	const { userId } = auth();
+
+	if (!userId) {
+		return redirect("/");
+	}
+
+	const { completedCourses, coursesInProgress } = await getDashboardCourses(userId);
+
+	return (
+		<div className="space-y-4 p-6">
+			<div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+				<InformationCard
+					icon={Clock}
+					label="In Progress"
+					numberOfItems={coursesInProgress.length}
+				/>
+
+				<InformationCard
+					icon={CheckCircle}
+					label="Completed"
+					numberOfItems={completedCourses.length}
+					variant="success"
+				/>
+			</div>
+
+			<CoursesList items={[...coursesInProgress, ...completedCourses]} />
+		</div>
+	);
 };
+
 export default HomePage;
